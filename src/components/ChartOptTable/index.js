015fbe94cab9df1a853482a5c9858e518c452f6a
luckynet@126.com <luckynet@126.com>
2020-04-23 10:37:49 +0800

    删除没用的问卷，控制压缩文件大小

--- src/components/ChartOptTable/index.js
+++ src/components/ChartOptTable/index.js
@@ -5,33 +5,6 @@ import * as utils from '../../utils/utils'
 import * as echarts from '../ec-canvas/echarts'
 import './index.scss'
 
-// function initChart(canvas, width, height) {
-//   const chart = echarts.init(canvas, null, {
-//     width: width,
-//     height: height
-//   })
-//   canvas.setChart(chart)
-
-//   const option = {
-//     tooltip: {},
-//     legend: {
-//       data: ['销量']
-//     },
-//     xAxis: {
-//       data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-//     },
-//     yAxis: {},
-//     series: [{
-//       name: '销量',
-//       type: 'bar',
-//       data: [5, 20, 36, 10, 10, 20]
-//     }]
-//   }
-
-//   chart.setOption(option)
-//   return chart
-// }
-
 class ChartOptTable extends Component {
 
   config = {
